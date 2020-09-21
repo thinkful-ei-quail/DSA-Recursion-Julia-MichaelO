75478c3bb4781d3f074385f423bad29b09d634a9
Michael Oldacre <mloldacre@live.com>
2020-09-21 12:32:39 -0400

    powerCalculator done

--- src/App.js
+++ src/App.js
@@ -5,19 +5,36 @@ function App() {
   function countSheep(sheep) {
     //Base Case
     if (sheep === 0) {
-      return "All sheep jumped over the fence"
+      return 'All sheep jumped over the fence \n'
     }
     //General Case
     else {
-      let str = `${sheep}: Another sheep jumps over the fence`;
+      let str = `${sheep}: Another sheep jumps over the fence \n`;
       return str + countSheep(sheep - 1)
+    }
+  }
+  
+  let expw = Math.pow(10,0)
+  
+  console.log(expw)
+  
+  function powerCalculator(base, exp) {
+    //Base Case
+    if(exp < 0) {
+      return 'Exponent should be >= 0 \n'
+    }
+    //General Case
+    else {
+      let ans = Math.pow(base, exp) + '\n'; 
+      return  ans + powerCalculator(base, exp - 1)     
     }
   }
   
   return (
     <div className="App">
     
-    {countSheep(3)}
+      <div>{countSheep(3)}</div>
+      <div>{powerCalculator(10, 2)}</div>
 
     </div>
   );
