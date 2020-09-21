55d6b2e30d9ed268383d2eacb7094a8628ccd2b3
Michael Oldacre <mloldacre@live.com>
2020-09-21 12:10:09 -0400

    countSheep done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function countSheep(sheep) {
+    //Base Case
+    if (sheep === 0) {
+      return "All sheep jumped over the fence"
+    }
+    //General Case
+    else {
+      let str = `${sheep}: Another sheep jumps over the fence`;
+      return str + countSheep(sheep - 1)
+    }
+  }
+  
   return (
     <div className="App">
-
-
+    
+    {countSheep(3)}
 
     </div>
   );
